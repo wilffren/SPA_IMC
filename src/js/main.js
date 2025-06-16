2527b151c9c62df61978fc221030c535399fca9b
Wilffren Efrain Muñoz Velasquez <wilffren14@gmail.com>
2025-06-16 18:43:50 -0500

    Feat: The basic spa was created

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
-// let myWeight 
-// let myHeight
-// let imc
+
+
 
 // myHeight = prompt(message = "please, write your height ")
 // myWeight = prompt(message = "please, write your weight ")
@@ -10,3 +9,19 @@
 // alert("your imc is: " + imc)
 
 // create a table with the clasification and write that information into the console
+
+
+function spaImc(){
+
+
+let myWeight = document.getElementById('weight').value
+
+let myHeight = document.getElementById('height').value
+
+let buttonCalculate = document.getElementById('calculate')
+
+buttonCalculate = myWeight / (myHeight * myHeight)
+
+let result = document.getElementById('result').innerHTML = buttonCalculate
+
+}
